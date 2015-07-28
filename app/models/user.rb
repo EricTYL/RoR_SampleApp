7b3df62fb509633100fcade1df5f9b817ff4ac4c
RickyTYL <rickytyl@icloud.com>
2015-07-28 19:57:50 +0800

    Define class methods by way1 in exercise 8.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,25 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+#  # Returns the hash digest of the given string.
+#  def User.digest(string)
+#    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : :BCrypt::Engine.cost
+#    BCrypt::Password.create(string, cost: cost)
+#  end
+#
+#  # Return a random token.
+#  def User.new_token
+#    SecureRandom.urlsafe_base64
+#  end
+
   # Returns the hash digest of the given string.
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : :BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # Return a random token.
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
