9b5e1af5fab432bd61c37dd9f9f3de01e7982192
RickyTYL <rickytyl@icloud.com>
2015-08-18 13:23:04 +0800

    Modify production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,9 @@ Rails.application.configure do
   #
   # SMTP setting for Gmail
   #
+  config.action_mailer.raise_delivery_errors = true
+  host = 'benefique-chaise-1992.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
