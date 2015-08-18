4a080a97e8ecd8b09e078339d71138ad9dfe3f65
RickyTYL <rickytyl@icloud.com>
2015-08-18 12:18:46 +0800

    Modify missing comma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "gmail.com",
-    :user_name            => ENV['sender_name']
-    :password             => ENV['sender_password']
+    :user_name            => ENV['sender_name'],
+    :password             => ENV['sender_password'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
