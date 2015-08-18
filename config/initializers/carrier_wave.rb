4aba3a5786071845554ec15fe01b86119f163183
RickyTYL <rickytyl@icloud.com>
2015-08-18 12:25:03 +0800

    Fix carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,8 @@
 if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Cloudinary
-      config.cloud_name => 'eric777love',
-      config.api_key    => ENV['cloudinary_api_key'],
-      config.api_secret => ENV['cloudinary_api_secret'],
-      config.cdn_subdomain => true
-    }
+  Cloudinary.config do |config|
+    config.cloud_name       = 'eric777love'
+      config.api_key        = ENV['cloudinary_api_key']
+      config.api_secret     = ENV['cloudinary_api_secret']
+      config.cdn_subdomain  = true
   end
 end
