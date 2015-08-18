d845624a9f04be749ae943d232bb4673d989879c
RickyTYL <rickytyl@icloud.com>
2015-08-18 14:36:58 +0800

    Comment fog method in uploader.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,8 +1,8 @@
 if Rails.env.production?
   Cloudinary.config do |config|
-    config.cloud_name       = 'eric777love'
-      config.api_key        = ENV['cloudinary_api_key']
-      config.api_secret     = ENV['cloudinary_api_secret']
-      config.cdn_subdomain  = true
+    config.cloud_name     = 'eric777love'
+    config.api_key        = ENV['cloudinary_api_key']
+    config.api_secret     = ENV['cloudinary_api_secret']
+    config.cdn_subdomain  = true
   end
 end
